7ade24353968e045e6221e86c8158a8a02b4078b
Iulia-Andreea20 <griandreea4@gmail.com>
2022-09-16 10:19:28 +0300

    refresh the page every 3 min

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ fetch('https://www.mocky.io/v2/58fda6ce0f0000c40908b8c8')
         for (i = 0; i < data.news.length; i++) {
             newsContainer.innerHTML += `
                 <h3>${data.news[i].title}</h3>
-                    <p>${data.news[i].details} <a href="url">...</a></p>
+                    <p>${data.news[i].details}<a href="url">...</a></p>
             `;
-
         }
     });
+
+window.setTimeout(function () { document.location.reload(true); }, 15000 * 4 * 3);
+
 
 
