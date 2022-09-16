ec3926b5bc36c4bd9a11ce35dc3538a24ad10203
Iulia-Andreea20 <griandreea4@gmail.com>
2022-09-16 10:08:51 +0300

    add links

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fetch('https://www.mocky.io/v2/58fda6ce0f0000c40908b8c8')
         for (i = 0; i < data.news.length; i++) {
             newsContainer.innerHTML += `
                 <h3>${data.news[i].title}</h3>
-                <p>${data.news[i].details}</p>
+                    <p>${data.news[i].details} <a href="url">...</a></p>
             `;
 
         }
